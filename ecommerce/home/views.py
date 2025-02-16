2591fc95b167ad29016d3a8db8e70f0d3e299c0c
jamil chowdhury <17-34747-2@student.aiub.edu>
2025-02-17 00:40:48 +0600

    url commit

--- ecommerce/home/views.py
+++ ecommerce/home/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, World!")
+    first_name = 'John'
+    last_name = 'Doe'
+    name = {'first_name': first_name, 'last_name': last_name}
+    return render(request, 'home/home.html', name)
+
